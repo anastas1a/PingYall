eeda595d4b81b9f77ffb2f3381a13db3b0c2d6e1
anastas1a <3ggrnaastoss@gmail.com>
2022-08-22 23:14:36 +0300

    emoji integration

--- handlers/pingall.py
+++ handlers/pingall.py
@@ -1,8 +1,11 @@
 from aiogram import types
 from pyrogram import types
 
+import random
 from misc import dp
 from utils import get_user_tag_list
+
+EMOJI_LIST = ["🇺🇦", "🔵", "🟡", "💛", "💙", "🟨", "🟦"]
 
 
 @dp.message_handler(lambda m: m.chat.type in ["group", "supergroup"], commands=["pingall"])
@@ -10,7 +13,11 @@ async def pingall_cmd(message: types.Message):
     arg = message.get_args() #аргумент з функції
     users_tag_list = await get_user_tag_list(message.chat.id)
 
+    generate = bool(arg)
     for users in users_tag_list:
+        if not generate:
+            arg = ''.join(random.sample(EMOJI_LIST, 5))
+
         await message.answer(
             arg +  "".join(users)
         )
--- utils/ping.py
+++ utils/ping.py
@@ -1,8 +1,8 @@
 from misc import app
 from config import USERS_CHUNK_SIZE
+import random
+
 
 async def get_user_tag_list(chat_id: int):
     users = [f"<a href='tg://user?id={user.user.id}'>\u2060</a>" async for user in app.get_chat_members(chat_id)] #список користувачів
     return [users[i:i + USERS_CHUNK_SIZE] for i in range(0, len(users), USERS_CHUNK_SIZE)]
-
-
