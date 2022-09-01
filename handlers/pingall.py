1abcbe17bb0b60e7516d2183dfaf4dfcdd538525
anastas1a <3ggrnaastoss@gmail.com>
2022-09-01 22:52:10 +0300

    del pingonline

--- handlers/pingall.py
+++ handlers/pingall.py
@@ -32,26 +32,3 @@ async def pingall_cmd(message: types.Message):
             arg +  "".join(users)
         )
         
-@dp.message_handler(commands=["pingonline"])
-async def pingonline_cmd(message: types.Message):
-    if message.chat.type not in ["group", "supergroup"]:
-        return await message.answer(
-            "Цією командою можна користуватися тільки в групових чатах!"
-        )
-        
-    arg = message.get_args() #аргумент з функції
-    if not (await message.bot.get_chat_member(message.chat.id, message.from_user.id)).status in ["creator", "administrator"]:
-        return await message.answer(
-            "Цією командою можуть користуватися лише адміністратори!"
-        )
-
-    users_tag_list = await get_user_online_tag_list(message.chat.id)
-    generate = not bool(arg)
-    for users in users_tag_list:
-        if generate:
-            arg = ''.join(random.sample(EMOJI_LIST, len(users)))
-
-        await message.answer(
-            arg +  "".join(users)
-        )
-        
